245d0e2d23242c150785b359cc5e151ed50fdf5e
penhao <penhao817@gmail.com>
2021-05-07 13:17:27 +0800

    [feat]  Dynamic 抓取 HTML baseHref

--- www/src/App.ts
+++ www/src/App.ts
@@ -1,12 +1,34 @@
 import * as _ from "lodash";
+import { Page } from "./pages/AbstractPage";
 import { IndexPage } from "./pages/IndexPage";
 import { AboutPage } from "./pages/AboutPage";
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    //
-    const indexPage = new IndexPage("index");
-    indexPage.init();
-    //
-    const aboutPage = new AboutPage("about");
-    aboutPage.init();
+    setBaseHref();
+    initPage();
 });
+
+const setBaseHref = () => {
+    let base = document.createElement("base");
+    base.href =
+        window.location.protocol +
+        "//" +
+        window.location.hostname +
+        (window.location.port ? ":" + window.location.port : "");
+    document.getElementsByTagName("head")[0].appendChild(base);
+};
+
+const initPage = () => {
+    const pageName = window.location.pathname
+        .split("/")
+        .pop()
+        .replace(".html", "");
+    let currentPage: Page;
+    switch (pageName) {
+        case "index":
+            currentPage = new IndexPage(pageName);
+        case "about":
+            currentPage = new AboutPage(pageName);
+    }
+    currentPage.init();
+};
--- www/src/pages/IndexPage.ts
+++ www/src/pages/IndexPage.ts
@@ -1,4 +1,4 @@
-import { greeting } from "./../components/Foo";
+import { greeting } from "../components/Foo";
 import { Page } from "./AbstractPage";
 export class IndexPage extends Page {
     constructor(name: string) {
