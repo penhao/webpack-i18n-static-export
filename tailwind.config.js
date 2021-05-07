78feccad761f43d047762ee766536bd2dca7c44b
penhao <penhao817@gmail.com>
2021-05-07 13:19:16 +0800

    [rewrite] tailwind.css theme設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,43 @@
 module.exports = {
-    purge: [],
+    purge: {
+        enabled: true,
+        content: ["./www/**/*.html"],
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
+        colors: {
+            transparent: "transparent",
+            black: "#000",
+            white: "#fff",
+            gray: {
+                100: "#f7fafc",
+                900: "#1a202c",
+            },
+        },
+        fontFamily: {
+            sans: ["Graphik", "sans-serif"],
+            serif: ["Merriweather", "serif"],
+        },
+        extend: {
+            spacing: {
+                128: "32rem",
+                144: "36rem",
+            },
+            borderRadius: {
+                "4xl": "2rem",
+            },
+        },
     },
     variants: {},
-    plugins: [],
+    plugins: [
+        require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/typography"),
+    ],
 };
